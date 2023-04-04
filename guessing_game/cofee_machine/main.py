003311bbbc0aeec4ac09d7a905d6a02785c74073
Michal <michal.kois@icloud.com>
2023-04-04 18:57:32 +0200

    new assignemnet

---
+++ guessing_game/cofee_machine/main.py
@@ -0,0 +1,44 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+###
+#1. Prompt user by asking “​What would you like? (espresso/latte/cappuccino):”​
+#   a. Check the user’s input to decide what to do next.
+#   b. The prompt should show every time action has completed, e.g. once the drink is
+#      dispensed. The prompt should show again to serve the next customer.
+
+def ask():
+    bewarege = input("​What would you like? (espresso/latte/cappuccino): ")
+    while bewarege in ["espresso", "latte", "cappuccino"]:
+        return bewarege
+    
+ask()
