06318af72a8c729baa08f194fefa406a37a1871e
Michal <michal.kois@icloud.com>
2023-04-12 17:47:15 +0200

    gmail working yahoo not

---
+++ email_smtp/main.py
@@ -0,0 +1,21 @@
+import smtplib, json
+
+with open("login_info.json","r") as f:
+    data = json.load(f)
+
+
+gmail_user = data["gmail"]["email"]
+gmail_pass = data["gmail"]["password"]
+
+yahoo_user = data["yahoo"]["email"]
+yahoo_pass = data["yahoo"]["password"]
+
+
+connection = smtplib.SMTP("smtp.gmail.com")
+#connection = smtplib.SMTP("smtp.mail.yahoo.com",port="587")
+connection.starttls() #securing email connection
+connection.login(user = gmail_user, password=gmail_pass)
+#connection.login(user = yahoo_user, password=yahoo_pass)
+
+
+connection.sendmail(from_addr=gmail_user, to_addrs=yahoo_user, msg="Hello2")
